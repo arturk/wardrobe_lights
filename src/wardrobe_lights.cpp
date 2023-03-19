b8c2c9b2b11a66333e65c717b3d83de237183fba
Artur Korobeynyk <artur.init@gmail.com>
2023-03-19 17:10:11 +0200

    Candle effect added

--- src/wardrobe_lights.cpp
+++ src/wardrobe_lights.cpp
@@ -1,34 +1,34 @@
 #include <Arduino.h>
 
-#define STRIP_SHELF_PIN 4     // пин полки
+#define STRIP_SHELF_PIN 11     // пин полки
 #define STRIP_WALL_PIN 3    // пин стенки
-#define NUMLEDS_SHELF 20
-#define WALL_STRIPS 1
-#define WALL_STRIP_LEDS 1
-#define TIMEOUT 5
-#define PIR_SENSOR 7
-#define BRIGHTNESS 150U
-#define SHELF_COLOR 0x4374e8
-#define WALL_COLOR 0xE91627
-#define SHELF_EFFECT_SPEED 1
-#define WALL_EFFECT_SPEED 3
+#define NUMLEDS_SHELF 20    // колличество лед секций в полке
+#define WALL_STRIPS 4     // колличество полосок в стенке
+#define WALL_STRIP_LEDS 2 // колличество лед секций в полоске стенки
+#define TIMEOUT 5   // время активности эффекта
+#define PIR_SENSOR 7   // пин сенсора движения
+#define BRIGHTNESS 150  // яркость эффектов
+#define SHELF_COLOR 0x4374e8  // цвет эффекта полки
+#define SHELF_EFFECT_SPEED 1  // скорость эффекта полки, чем выше значение, тем медленее
+#define WALL_EFFECT_SPEED 2   // скорость эффекта стенки, чем выше значение, тем медленее
 
 static uint32_t timeoutCounter;
 static bool active = true;
 
-static const byte brightness_delta = 1;
-static byte shelf_skip_counter = SHELF_EFFECT_SPEED;
+static const uint8_t brightness_delta = 1;
+static uint8_t shelf_skip_counter = SHELF_EFFECT_SPEED;
 static uint8_t shelf_counter_brightness = 0;
 
-static uint8_t wall_counter_brightness = 0;
-static byte wall_skip_counter = WALL_EFFECT_SPEED;
+static uint8_t wall_skip_counter = WALL_EFFECT_SPEED;
+static uint8_t current_wall_strip = 0;
+static uint8_t current_wall_led = 0;
 
 #define COLOR_DEBTH 3
 #include <microLED.h>   // подключаем библу
 microLED<NUMLEDS_SHELF, STRIP_SHELF_PIN, MLED_NO_CLOCK, LED_WS2818, ORDER_GRB, CLI_AVER> strip_shelf;
 microLED<WALL_STRIPS * WALL_STRIP_LEDS, STRIP_WALL_PIN, MLED_NO_CLOCK, LED_WS2818, ORDER_GRB, CLI_AVER> strip_wall;
 
-void brightness_counter(uint8_t effect_speed, uint8_t& skip_counter, uint8_t& brightness_counter){
+void brightness_increase_filler(uint8_t effect_speed, uint8_t& skip_counter, uint8_t& brightness_counter){
   if(skip_counter > 0) {
     skip_counter--;
     return;
@@ -53,15 +53,42 @@ void brightness_counter(uint8_t effect_speed, uint8_t& skip_counter, uint8_t& br
   }
 }
 
+void candle_filler(){
+  if(active) {
+    strip_wall.setBrightness(BRIGHTNESS);
+    mData color = mRGB(160, 50, 160);
+    if(current_wall_led != 0 && current_wall_led % WALL_STRIP_LEDS == 0) {
+        current_wall_strip += 1;
+        current_wall_led = 0;
+    }
+    if(current_wall_strip > WALL_STRIPS - 1){
+      current_wall_strip = 0;
+    }
+    if(current_wall_led % WALL_STRIP_LEDS-1 == 0){
+      color = mRGB(random(120, 255), 0, random(10, 50));
+    }
+    strip_wall.set(current_wall_strip * WALL_STRIP_LEDS + current_wall_led, color);
+    current_wall_led+=1;
+  } else {
+    strip_wall.clear();
+    current_wall_led = 0;
+    current_wall_strip = 0;
+  }
+}
+
 void shelf_filler() {
-  brightness_counter(SHELF_EFFECT_SPEED, shelf_skip_counter, shelf_counter_brightness);
+  brightness_increase_filler(SHELF_EFFECT_SPEED, shelf_skip_counter, shelf_counter_brightness);
   strip_shelf.setBrightness(shelf_counter_brightness);
   strip_shelf.show();
 }
 
 void wall_filler() {
-  brightness_counter(WALL_EFFECT_SPEED, wall_skip_counter, wall_counter_brightness);
-  strip_wall.setBrightness(wall_counter_brightness);
+  if(wall_skip_counter > 0) {
+    wall_skip_counter--;
+    return;
+  }
+  wall_skip_counter = WALL_EFFECT_SPEED;
+  candle_filler();
   strip_wall.show();
 }
 
@@ -86,7 +113,7 @@ void setup() {
   pinMode(PIR_SENSOR, INPUT);
   strip_shelf.fill(SHELF_COLOR);
   strip_shelf.setBrightness(0);
-  strip_wall.fill(WALL_COLOR);
+  strip_wall.setBrightness(0);
 }
 
 void loop() {
